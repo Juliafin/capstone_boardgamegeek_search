ddcaf1d5c9f7757b37a6753373bbf885b9dad418
Juliafin <juliafinarovsky@gmail.com>
2017-02-25 23:20:13 -0500

    updated new user flow for project

--- prototypes/streamfinder_api/streamfinder.js
+++ prototypes/streamfinder_api/streamfinder.js
@@ -5,14 +5,14 @@
 http://streamfinder.com/api/?api_codekey=mYRQDsM0Kaca6Kf1aOthIjaykucbtrqlPfLV3T9nVhNB4aSy9UNvkzMJrQsanhQ5fLlLcPpe2S5S7Yu0Ha4m7vKNzj8SiYaTobKrQhKOziXjblsPZ6CG1Qv8uqt9URIw&return_data_format=json&do=get_genre_list&format=mp3
 
 // accesskey mYRQDsM0Kaca6Kf1aOthIjaykucbtrqlPfLV3T9nVhNB4aSy9UNvkzMJrQsanhQ5fLlLcPpe2S5S7Yu0Ha4m7vKNzj8SiYaTobKrQhKOziXjblsPZ6CG1Qv8uqt9URIw
-var OMDB_BASE_URL = "https://streamfinder.com/api/index.php"
+var STREAMFINDER_BASE_URL = "https://streamfinder.com/api/index.php"
 function getDataFromStreamfinderApi(callback) {
 
   var streamfinderSendSetting = {
-    url: OMDB_BASE_URL,
+    url: STREAMFINDER_BASE_URL,
     data: {
       api_codekey: 'mYRQDsM0Kaca6Kf1aOthIjaykucbtrqlPfLV3T9nVhNB4aSy9UNvkzMJrQsanhQ5fLlLcPpe2S5S7Yu0Ha4m7vKNzj8SiYaTobKrQhKOziXjblsPZ6CG1Qv8uqt9URIw',
-      return_data_format: 'json',
+      return_data_format: 'jsonp',
       do: 'get_genre_list',
       num: '10',
       format: 'mp3',
